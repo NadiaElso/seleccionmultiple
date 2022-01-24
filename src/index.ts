47a9a3c36fe53dacecce4c81ca542e2eabe63105
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-24 14:49:29 -0300

    actualizado

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,31 @@
-let sueldoActual:number=Number(prompt("Ingrese su sueldo actual"));
-let sueldoConAumento:number;
-let descuento:number;
-
-
+let sueldoActual: number = Number(prompt("Ingrese su sueldo actual"));
+let sueldoConAumento: number;
+let aumento: number;
 
 switch (true) {
-  case  sueldoActual>0&&sueldoActual<=15000:
-    descuento=sueldoActual*0.20;
-    sueldoActual=sueldoActual-descuento;
-    console.log(sueldoActual);
+  case sueldoActual > 0 && sueldoActual <= 15000:
+    aumento = sueldoActual * 0.2;
+    sueldoActual = sueldoActual + aumento;
+    console.log("Sueldo con aumento" + sueldoActual);
     break;
 
-    case 1 sueldoActual>15001&&sueldoActual<=20000:
-        descuento=sueldoActual*0.10;
-        sueldoActual=sueldoActual-descuento;
-        console.log(sueldoActual);
+  case sueldoActual >= 15001 && sueldoActual <= 20000:
+    aumento = sueldoActual * 0.1;
+    sueldoActual = sueldoActual + aumento;
+    console.log("Sueldo con aumento" + sueldoActual);
     break;
 
-        case 2 sueldoActual>20001&&sueldoActual<=25000:
-            descuento=sueldoActual*0.5;
-            sueldoActual=sueldoActual-descuento;
-            console.log(sueldoActual);
+  case sueldoActual >= 20001 && sueldoActual <= 25000:
+    aumento = sueldoActual * 0.5;
+    sueldoActual = sueldoActual + aumento;
+    console.log("Sueldo con aumento" + sueldoActual);
     break;
 
-            case 3 sueldoActual>25000:
-                descuento=sueldoActual*0.20;
-                sueldoActual=sueldoActual-descuento;
-                console.log(sueldoActual);
-                            
+  case sueldoActual >= 25001:
+    console.log("No hay aumento");
+
     break;
-console.log("No hay aumento");
+
   default:
     break;
 }
